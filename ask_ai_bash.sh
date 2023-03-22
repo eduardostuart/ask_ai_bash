a64ce2230049dbd38d2537b53bab745941236b0a
Eduardo Stuart <hi@s.tuart.me>
2023-03-22 17:32:11 +0100

    fixes

--- ask_ai_bash.sh
+++ ask_ai_bash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ask_ai_error() {
+_ask_ai_error() {
   echo "Error: $@" >&2
 }
 
@@ -9,21 +9,23 @@ ask_ai() {
   NC='\033[0m'
 
   if [ ! -x "$(which curl)" ] ; then 
-    ask_ai_error "curl is not installed"  
+    _ask_ai_error "curl is not installed"  
     return
   fi
   if [ ! -x "$(which jq)" ] ; then 
-    ask_ai_error "jq is not installed"  
+    _ask_ai_error "jq is not installed"  
     return
   fi
   if [ -z "${OPENAI_APIKEY}" ]; then 
-    ask_ai_error "Error:  OPENAI_APIKEY is not set or empty" 
+    _ask_ai_error "OPENAI_APIKEY is not set or empty" 
     return
   fi    
 
+  local prompt
   echo "${CYAN}Insert question:${NC}"
   read prompt
 
+  local data
   data='{
     "prompt": "'$prompt'",
     "model": "text-davinci-003",
@@ -33,6 +35,7 @@ ask_ai() {
     "max_tokens": 1000
   }'
 
+  local output
   output=$(
       curl -sS -X POST \
         -H  "Authorization: Bearer $OPENAI_APIKEY" \
